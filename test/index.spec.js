ef8f0c17fc49e2aaa4041b1ad2ac85ece4492e8b
Daksh Miglani <hello@dak.sh>
2017-08-04 21:56:58 +0530

    added tests with fixes.

---
+++ test/index.spec.js
@@ -0,0 +1,19 @@
+var main = require('../index')
+const expect = require('chai').expect
+
+describe('Idle Function', () => {
+    it('should export a function', () => {
+        expect(main).to.be.a('function')
+    })
+})
+
+describe('Start Idling', () => {
+    it('should export an object', () => {
+        expect(main({
+            username: '123',
+            password: '123',
+            twoFactorCode: 'gg',
+            gameId: '123'
+        })).to.be.a('object')
+    })
+})
